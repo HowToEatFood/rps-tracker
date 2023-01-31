ea08ded31831fb7db2db2fe23b4abf52be096b5f
HowToEatFood <122560188+HowToEatFood@users.noreply.github.com>
2023-01-31 13:34:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    Player_A += 1
+    rounds += 1
+    update_scoreboard()
+})
 function update_scoreboard () {
     OLED.clear()
     OLED.writeStringNewLine("player A" + Player_A)
-    OLED.writeStringNewLine("player B" + Ties)
-    OLED.writeStringNewLine("ties" + Player_B)
+    OLED.writeStringNewLine("player B" + Player_B)
+    OLED.writeStringNewLine("ties" + Ties)
     OLED.writeStringNewLine("rounds" + rounds)
 }
+input.onButtonPressed(Button.AB, function () {
+    Ties += 1
+    rounds += 1
+    update_scoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    Player_B += 1
+    rounds += 1
+    update_scoreboard()
+})
 input.onGesture(Gesture.Shake, function () {
     reset()
 })
@@ -18,8 +33,8 @@ function reset () {
     basic.pause(2000)
     update_scoreboard()
 }
-let rounds = 0
-let Player_B = 0
 let Ties = 0
+let Player_B = 0
+let rounds = 0
 let Player_A = 0
 reset()
