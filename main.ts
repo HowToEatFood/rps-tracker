f1886218717a74a4ee002e3bdf7db57a5a4dfd7c
HowToEatFood <122560188+HowToEatFood@users.noreply.github.com>
2023-01-31 13:25:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,14 +5,21 @@ function update_scoreboard () {
     OLED.writeStringNewLine("ties" + Player_B)
     OLED.writeStringNewLine("rounds" + rounds)
 }
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Lets play!")
+    rounds = 0
+    Player_B = 0
+    Player_A = 0
+    Ties = 0
+    basic.pause(2000)
+    update_scoreboard()
+}
+let rounds = 0
+let Player_B = 0
 let Ties = 0
 let Player_A = 0
-let Player_B = 0
-let rounds = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Lets play!")
-rounds = 0
-Player_B = 0
-Player_A = 0
-Ties = 0
-basic.pause(2000)
+reset()
